41a7d5a5a7cda3c90e83072f48aa95824b447b2b
Suncoin Devs <bitcoin.suncoin@mailanyway.com>
2014-02-18 08:04:11 +1100

    checkpoints added

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -25,6 +25,9 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of // Checkpoint 0 hash == Genesis block hash.
         (         0, uint256("0xe9fa4ca452a0fb59f8273fdd73c4dee3cf9b17d615f9e0b24f0b9bd20281f9b3"))
+        (      5000, uint256("0xc42b03e4bfc90af8682f97d65d40d539591960234a2e26c8b261f5d85db407bd"))
+        (     10000, uint256("0x3a6fbf9f8ffa0523bab1709117a7e62fb3b0223e8861aa5539d9b4047beee117"))
+        (     15000, uint256("0x9b97f2e9af91b5a9d1bdb4fc7b2cf182ec7df010f446e362a614f76c28b62ff7"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
